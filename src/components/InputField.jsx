e0022e0ad39674a03982655927a792516ea3ef79
Husnain Aslam <husnain.aslam@brainxtech.com>
2024-07-19 17:34:23 +0500

    Adds input form to get task

---
+++ src/components/InputField.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Button from "./Button";
+
+export default function InputField({ addTodo }) {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(task);
+    setTask("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="grid grid-flow-col gap-8 mb-8 text-black"
+    >
+      <input
+        type="text"
+        className="rounded-2xl p-2 col-span-5"
+        placeholder="Type something"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <Button type="submit">Add</Button>
+    </form>
+  );
+}
